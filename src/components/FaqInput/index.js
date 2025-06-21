a06f99e7f1d32e16cf6d09094428fd0b546907d3
Piyussssh <mvps1862@gmail.com>
2025-06-21 10:09:14 +0530

    temp

--- src/components/FaqInput/index.js
+++ src/components/FaqInput/index.js
@@ -1,18 +1,37 @@
 "use client";
+import { useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 export default function FaqInput() {
+  const [faq, setFaq] = useState({ que: "", ans: "" });
+
+  const handleQuestionChange = (e) => {
+    setFaq({ ...faq, que: e.target.value });
+  };
+
+  const handleAnswerChange = (e) => {
+    setFaq({ ...faq, ans: e.target.value });
+  };
+
+  const handleSave = () => {
+  };
   return (
     <div className="py-4 cont gap-4">
       <input
+        value={faq.question}
+        onChange={handleQuestionChange}
         className="input w-full focus:outline-none input-lg text-xl"
         placeholder="Question..."
       />
       <TextareaAutosize
+        value={faq.answer}
+        onChange={handleAnswerChange}
         minRows={3}
         className="input w-full focus:outline-none input-lg py-1 text-lg resize-none"
-        placeholder="Answere..."
+        placeholder="Answer..."
       />
-      <button className="btn btn-primary btn-base w-fit">Save FAQ</button>
+      <button onClick={handleSave} className="btn btn-primary btn-base w-fit">
+        Save FAQ
+      </button>
     </div>
   );
 }
